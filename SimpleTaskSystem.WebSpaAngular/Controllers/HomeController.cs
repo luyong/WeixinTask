f79c82496c5711b16dcf25eda7f64b6547186c0b
luyong <284058698@qq.com>
2016-06-13 16:31:19 +0800

    初始化，weixinsdk和tasksystem合并

---
+++ SimpleTaskSystem.WebSpaAngular/Controllers/HomeController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+using Deepleo.Weixin.SDK;
+using SimpleTaskSystem.WebSpaAngular.Attributes;
+
+namespace SimpleTaskSystem.WebSpaAngular.Controllers
+{
+    [WeixinOAuthAuthorize]
+    public class HomeController : SimpleTaskSystemControllerBase
+    {
+        public ActionResult Index()
+        {
+            return View("~/App/Main/views/layout/layout.cshtml"); //Layout of the angular application.
+        }
+	}
+}
